ceee46dcefae561af4b316582acbd1462962ea84
JeffLeite91 <jefrey_jele@hotmail.com>
2025-01-09 22:21:21 -0300

    first commit

---
+++ exemplo_01.py
@@ -0,0 +1,16 @@
+#PRIMEIRA AULA DE PYTHON 
+
+id = 0
+name = ""
+salario = 0.0
+brasilero = True
+
+id = int (input ("Informe sua ID: "))
+name = str (input ("Informe seu nome: "))
+salario = float (input ("Informe seu salario: "))
+brasileiro = bool (input("Você é brasileiro?"))
+
+print("Sua id é: ", id)
+print("Bem vindo ", name)
+print("Seu salario é: ", salario)
+print("BR?",brasileiro)
